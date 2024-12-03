773d5159ae7413a4501d2b0cc3459f2297a8a91d
grairudolf <153108176+grairudolf@users.noreply.github.com>
2024-12-03 08:13:31 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 
 $to = "rtech777r@gmail.com";
-$subject = "Mail From Quest Qduc";
+$subject = "Mail From Quest Educ";
 $txt ="Name = ". $name . "\r\n Email = " . $email."\r\n message=" . $message.
 
 $header = "From: noreply@https://questeduc-theta.vercel.app" . "\r\n" .
